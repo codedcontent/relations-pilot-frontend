53c6ab9ba8ac8e45623125bd477c87936eec2f26
unknown <ogemephors002@gmail.com>
2024-01-30 17:41:49 +0000

    Fixed .env problem

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 
 const constants = {
-  serverURL: `${process.env.SERVER_URL}/api`,
+  serverURL: `${import.meta.env.VITE_SERVER_URL}/api`,
 };
 
 export default constants;
